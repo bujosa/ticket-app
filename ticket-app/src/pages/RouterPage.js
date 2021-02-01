d7e97952cad9f8e2e5edc6eec7283b434e107152
bujosa <davidbujosa@gmail.com>
2021-02-01 17:24:24 -0400

    Added new properties on router page

--- ticket-app/src/pages/RouterPage.js
+++ ticket-app/src/pages/RouterPage.js
@@ -6,36 +6,44 @@ import {
   UploadOutlined,
 } from "@ant-design/icons";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import { SignIn } from "./SignIn";
+
 const { Sider, Content } = Layout;
 
 export const RouterPage = () => {
   return (
-    <Layout style={{ height: "100vh" }}>
-      <Sider>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            SignIn
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            Tail
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            Create Ticket
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Content
-          className="site-layout-background"
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-          }}>
-          Content
-        </Content>
+    <Router>
+      <Layout style={{ height: "100vh" }}>
+        <Sider>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              <Link to="/signin">SignIn</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              <Link to="/tail">Tail</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              <Link to="/create-ticket">Create Ticket</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+            }}>
+            <Switch>
+              <Route path="/signin" component={SignIn} />
+            </Switch>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </Router>
   );
 };
