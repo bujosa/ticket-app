bec0890412935dfd642b743e6d61c771fe11436c
bujosa <davidbujosa@gmail.com>
2021-02-01 19:22:08 -0400

    Added properties on tail page

--- ticket-app/src/pages/Tail.js
+++ ticket-app/src/pages/Tail.js
@@ -1,5 +1,88 @@
 import React from "react";
 
+import { Col, Typography, Row, List, Card, Tag, Divider } from "antd";
+
+const { Text, Title } = Typography;
+
+const data = [
+  {
+    ticketNo: 33,
+    escritorio: 3,
+    agente: "Fernando Herrera",
+  },
+  {
+    ticketNo: 34,
+    escritorio: 4,
+    agente: "Melissa Flores",
+  },
+  {
+    ticketNo: 35,
+    escritorio: 5,
+    agente: "Carlos Castro",
+  },
+  {
+    ticketNo: 36,
+    escritorio: 3,
+    agente: "Fernando Herrera",
+  },
+  {
+    ticketNo: 37,
+    escritorio: 3,
+    agente: "Fernando Herrera",
+  },
+  {
+    ticketNo: 38,
+    escritorio: 2,
+    agente: "Melissa Flores",
+  },
+  {
+    ticketNo: 39,
+    escritorio: 5,
+    agente: "Carlos Castro",
+  },
+];
+
 export const Tail = () => {
-  return <div> Tail Page</div>;
+  return (
+    <>
+      <Title level={1}>Serving the client</Title>
+      <Row>
+        <Col span={12}>
+          <List
+            dataSource={data.slice(0, 3)}
+            renderItem={(item) => (
+              <List.Item>
+                <Card
+                  style={{ width: 300, marginTop: 16 }}
+                  actions={[
+                    <Tag color="volcano">{item.agente}</Tag>,
+                    <Tag color="volcano">Escritorio: {item.escritorio}</Tag>,
+                  ]}>
+                  <Title>No. {item.ticketNo}</Title>
+                </Card>
+              </List.Item>
+            )}
+          />
+        </Col>
+        <Col span={12}>
+          <Divider>Historial</Divider>
+          <List
+            dataSource={data.slice(3)}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  title={`Ticket No. ${item.ticketNo}`}
+                  description={
+                    <>
+                      <Text type="secondary">In the Desktop</Text>
+                    </>
+                  }
+                />
+              </List.Item>
+            )}
+          />
+        </Col>
+      </Row>
+    </>
+  );
 };
