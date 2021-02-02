19fc02ad1ad43da5966b4d80354cec810d745d9b
bujosa <davidbujosa@gmail.com>
2021-02-01 21:40:10 -0400

    Added socket provider on  TicketApp

--- ticket-server/models/server.js
+++ ticket-server/models/server.js
@@ -1,41 +1,41 @@
 // Servidor de Express
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
-const path = require('path');
-const Sockets = require('./sockets');
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+const Sockets = require("./sockets");
 
 class Server {
-	constructor() {
-		this.app = express();
-		this.port = process.env.PORT;
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
 
-		this.server = http.createServer(this.app);
+    this.server = http.createServer(this.app);
 
-		this.io = socketio(this.server, {
-			/* Configurations */
-		});
-	}
+    this.io = socketio(this.server, {
+      /* Configurations */
+    });
+  }
 
-	middlewares() {
-		this.app.use(express.static(path.resolve(__dirname, '../public')));
+  middlewares() {
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
 
-		// this.app.use(cors());
-	}
+    // this.app.use(cors());
+  }
 
-	configurarSockets() {
-		new Sockets(this.io);
-	}
+  configurarSockets() {
+    new Sockets(this.io);
+  }
 
-	execute() {
-		this.middlewares();
+  execute() {
+    this.middlewares();
 
-		this.configurarSockets();
+    this.configurarSockets();
 
-		this.server.listen(this.port, () => {
-			console.log('Server corriendo en puerto:', this.port);
-		});
-	}
+    this.server.listen(this.port, () => {
+      console.log("Server corriendo en puerto:", this.port);
+    });
+  }
 }
 
 module.exports = Server;
--- ticket-server/models/sockets.js
+++ ticket-server/models/sockets.js
@@ -1,43 +1,18 @@
-const BandList = require(`./band-list`);
-
 class Sockets {
-	constructor(io) {
-		this.io = io;
+  constructor(io) {
+    this.io = io;
 
-		this.bandList = new BandList();
+    this.socketEvents();
+  }
 
-		this.socketEvents();
-	}
+  socketEvents() {
+    // On connection
+    this.io.on("connection", (socket) => {
+      console.log("Cliente conectado");
+    });
 
-	socketEvents() {
-		// On connection
-		this.io.on('connection', (socket) => {
-			console.log('Cliente conectado');
-			socket.emit('current-bands', this.bandList.getBands());
-
-			socket.on('vote-band', (id) => {
-				this.bandList.increaseVotes(id);
-				this.io.emit('current-bands', this.bandList.getBands());
-			});
-
-			socket.on('delete-band', (id) => {
-				this.bandList.removeBand(id);
-				this.io.emit('current-bands', this.bandList.getBands());
-			});
-
-			socket.on('change-name-band', (data) => {
-				this.bandList.changeName(data.id, data.name);
-				this.io.emit('current-bands', this.bandList.getBands());
-			});
-
-			socket.on('add-band', (name) => {
-				this.bandList.addband(name);
-				this.io.emit('current-bands', this.bandList.getBands());
-			});
-		});
-
-		// vote band
-	}
+    // vote band
+  }
 }
 
 module.exports = Sockets;
